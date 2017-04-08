87e38395c21d7732de45b929a6506f8343a9f674
d0omiel <danielcalustro@gmail.com>
2017-04-08 01:17:42 -0400

    my new edit
    trying to make  it all in one line. i like it that

--- src/card.java
+++ src/card.java
@@ -1,9 +1,7 @@
 
 public class card {
 	private static String[] Suit = {"diamond", "club", "heart", "spade"};
-	private static final String[] Rank = {"Ace", "2", "3", "4", "5",
-										  "6", "7", "8", "9", "10",
-										  "Jack", "Queen", "King"};
+	private static final String[] Rank = {"Ace", "2", "3", "4", "5","6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 	private int value;
 	card() {
 		
